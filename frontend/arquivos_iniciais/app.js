2d5223a689a83af593eb7c1c768c8a14eb958c40
ASAngelica <asa196549@gmail.com>
2021-05-17 19:59:17 -0300

    Removendo despesas e considerações finais

--- frontend/arquivos_iniciais/app.js
+++ frontend/arquivos_iniciais/app.js
@@ -56,9 +56,12 @@ class Bd {
             //recuperar a despesa
             let despesa = JSON.parse(localStorage.getItem(i))
 
+            //existe a possibilidade de haver índices que foram pulados/removidos
+            //nestes casos nós vamos pular esses índices
             if(despesa === null) {
                 continue
             }
+            despesa.id = i
             despesas.push(despesa)
         }
         return despesas
@@ -114,6 +117,10 @@ class Bd {
         }
 
         return despesasFiltradas
+    }
+
+    remover(id){
+        localStorage.removeItem(id)
     }
 }
 
@@ -215,6 +222,23 @@ function carregaListaDespesas(despesas = Array(), filtro = false){
         linha.insertCell(1).innerHTML = d.tipo
         linha.insertCell(2).innerHTML = d.descricao
         linha.insertCell(3).innerHTML = d.valor
+
+        //criar o botão de exclusão
+        let btn = document.createElement("button")
+        btn.className = 'btn btn-danger'
+        btn.innerHTML = '<i class="fas fa-times"></i>'
+        btn.id = `id_despesa_${d.id}`
+        btn.onclick = function(){
+            //remover a despesa
+
+            let id = this.id.replace('id_despesa_', '')
+
+            bd.remover(id)
+
+            window.location.reload()
+        }
+        linha.insertCell(4).append(btn)
+        console.log(d)
     })
 }
 
